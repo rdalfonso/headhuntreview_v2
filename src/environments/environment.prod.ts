ee61f233500975a67106364f7f500c3a4e9aa422
Richard D'Alfonso <rdalfonso@gmail.com>
2018-08-22 14:57:05 -0500

    config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  apiEndpoint: 'http://34.200.225.46:5002/api',
+  apiMapUrl: 'https://www.google.com/maps/embed/v1/place?key=',
+  apiMapKey: 'AIzaSyDNubxaoKzoVZ554NYuPJ4kDGEZWaRx1Tw',
+  authDomain: 'headhunterreviews-ffb31.firebaseapp.com',
+  fbDatabaseUrl: 'https://headhunterreviews-ffb31.firebaseio.com',
+  projectId: "headhunterreviews-ffb31",
+  storageBucket: "headhunterreviews-ffb31.appspot.com",
+  messagingSenderId: "941588946487"
+};
