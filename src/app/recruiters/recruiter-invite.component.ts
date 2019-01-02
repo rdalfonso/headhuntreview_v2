a06aa20cacb5f2c8a511fadcac460f7012061b9f
Richard D'Alfonso <rdalfonso@gmail.com>
2019-01-02 09:32:39 -0600

    Fix for ng serve

--- src/app/recruiters/recruiter-invite.component.ts
+++ src/app/recruiters/recruiter-invite.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router, Params  } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { Recruiter } from '../models/recruiter.model';
 import { RecruiterService } from './recruiter.service';
+import { ICompany } from '../companies/company';
+import { CompanyService } from '../companies/company.service';
 import { UserService } from './../admin/adminShared/user.service';
 import { Observable, Subject } from 'rxjs/';
 
@@ -12,8 +14,13 @@ import { Observable, Subject } from 'rxjs/';
 })
 export class RecruiterInviteComponent implements OnInit {
 
+  public companies: Observable<any[]>;
+  private searchTerms = new Subject<string>();
+  public CompanyName = '';
+  public flag = true;
   IsLoggedin = false;
   redirect = '/recruiters';
+  filteredCompanies: ICompany[];
   model = new Recruiter('', '', '', '', '', 0, '', '');
   experienceLevels = ['Junior', 'Mid-level', 'Senior'];
 
@@ -21,17 +28,56 @@ export class RecruiterInviteComponent implements OnInit {
     private _router: Router,
     private _activatedRoute: ActivatedRoute,
     private _recruiterService: RecruiterService,
+    private _companyService: CompanyService,
     private _userService: UserService) {
   }
 
   ngOnInit(): void {
     this.IsLoggedin = this._userService.verifyAuth();
+    const param = this._activatedRoute.snapshot.paramMap.get('id');
+    if (param) {
+      this.getCompany(param);
+    }
 
     this._activatedRoute.queryParams
       .filter(params => params.mode)
       .subscribe(params => {
         this.redirect = '/' + params.mode + this.redirect;
       });
+
+    this.companies = this.searchTerms
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .switchMap(term => term ? this._companyService.searchCompanies(term) : Observable.of<ICompany[]>([]))
+      .catch(error => {
+        console.log(error);
+        return Observable.of<ICompany[]>([]);
+      });
+  }
+
+  searchCompanies(term: string): void {
+    this.flag = true;
+    this.searchTerms.next(term);
+  }
+
+  onselectCompany(company) {
+    if (company) {
+      this.model.companyName = company.name;
+      this.model.companyId = company.uniqueId;
+      this.flag = false;
+    }
+  }
+
+  getCompany(id: string) {
+    this._companyService.getCompany(id).subscribe(
+      (company) => {
+        if (company) {
+          this.model.companyName = company.name;
+          this.model.companyId = company.uniqueId;
+          this.flag = false;
+        }
+      },
+      error => console.log(error));
   }
 
   nameToUpperCase(value: string) {
@@ -41,6 +87,14 @@ export class RecruiterInviteComponent implements OnInit {
        this.model.name = value;
      }
     }
+
+  ratingComponetClick(clickObj: any): void {
+        console.log(`The Item ${clickObj.itemId}
+            has been given ${clickObj.rating} stars,
+            now is time to update the item with the new rating`);
+
+            this.model.stars = clickObj.rating;
+  }
 
   onBack(): void {
     this._router.navigate([this.redirect]);
